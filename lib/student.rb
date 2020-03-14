d8788ebb8870ba1b7cd6df0cb6f4e2b3f4f3bbbc
arnie b <arnold.bwaila@gmail.com>
2020-03-14 22:52:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,5 +35,7 @@ end
       INSERT INTO students (name, grade) 
       VALUES (?, ?)
     SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
 
 end
