f29fc45082fe1fb9d7d15b56e7db38716e94e547
arnie b <arnold.bwaila@gmail.com>
2020-03-14 23:00:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,23 +44,24 @@ end
     save
   end
   
-def self.new_from_db(row)
-  new_student = self.new  
-  new_student.id = row[0]
-  new_student.name =  row[1]
-  new_student.grade = row[2]
-  new_student
-end
+  def self.new_from_db(row)
+    new_student = self.new  
+    new_student.id = row[0]
+    new_student.name =  row[1]
+    new_student.grade = row[2]
+    new_student
+  end
   
-def self.find_by_name(name)
-    sql = <<-SQL
-        SELECT * 
-        FROM students
-        WHERE students.name = ?
-        SQL
-        DB[:conn].execute(sql,name).map do |row|
-          self.new_from_db(row)
-       end.first
+  def self.find_by_name(name)
+      sql = <<-SQL
+          SELECT * 
+          FROM students
+          WHERE students.name = ?
+          SQL
+          DB[:conn].execute(sql,name).map do |row|
+            self.new_from_db(row)
+         end.first
+  end
 end
 
 
