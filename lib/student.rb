d186ae39a68e5dbec1c350abc06090203889f91b
arnie b <arnold.bwaila@gmail.com>
2020-03-14 22:56:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,6 +39,13 @@ end
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
   
+  def self.new_from_db(row)
+  new_student = self.new  
+  new_student.id = row[0]
+  new_student.name =  row[1]
+  new_student.grade = row[2]
+  new_student
+end
   
 
 end
